d7c7a46dca345bc67de357de1c2c4277c61ed6f3
Raul Ungureanu <munchausend@gmail.com>
2021-06-13 19:38:11 +0100

    Migrate get_ai from notebooks to R scripts
    Script gettingAI.R is sources in get_ai.Rmd

---
+++ Methods/Scripts/Surveys/gettingAI.R
@@ -0,0 +1,178 @@
+
+# Setup 
+
+# Clear env
+rm(list = ls())
+
+# Load packages
+library(readxl)
+library(tidyverse)
+library(data.table)
+library(glue)
+library(lubridate)
+library(docstring)
+library(glue)
+
+# Set root directory.
+rootDir <- '/Volumes/Survey_Social_Media_Compare'
+
+# Load sheet holding survey periods
+s_periods_fname <- "Methods/Scripts/Surveys/table_details/surveyPeriods.xlsx"
+
+surveyPeriods <- read_excel(file.path(rootDir, s_periods_fname), 
+                            sheet = "AI+HPS", 
+                            col_types = c("text", 
+                                          "date", 
+                                          "date", 
+                                          "text", 
+                                          "date", 
+                                          "date", 
+                                          "text",
+                                          "text",
+                                          "text"))
+
+# Get month and day when of the A/I period end (this is how files are named)
+ai_dates <- paste0(month(surveyPeriods$A_I_end_date,label = TRUE, abbr = FALSE), " " ,day(surveyPeriods$A_I_end_date))
+
+# Make df with the corresponding HPS period + the date identified above.
+# Will use this to find the correct files.
+ai_periods <- data.frame(Period = surveyPeriods$Period, 
+                         Date = ai_dates)
+
+# Get the correct sheets from the A/I survey data
+
+# Functions.
+# Geting the employment data.
+getEmp <- function(dir_path, file){
+  #' Getting the employment sheet from the Axios-Ipsos survey data for a specific A/I "week"
+  #' Input: dir_path (chr), path to the data.
+  #'        file (chr), file name.
+  #' Output: df (data.frame), dataframe of the sheet containing the employment data from one A/I "week" 
+  
+  # Specify full path.
+  fpath <- file.path(dir_path, file)
+  
+  # Read the correct sheet.
+  # The naming (for the relevant sheet) changed from "PPWORK" to "PPEMPLOY", early on.
+  # Only one of these will be present, so 2 try's seems like the most straightforward way to handle it.
+  # TODO: re-write using purr's possibly()?
+  try(df <- read_excel(fpath,
+                       sheet = "PPEMPLOY", 
+                       skip = 9, 
+                       n_max = 19), silent = TRUE)
+  
+  try(df <- read_excel(fpath,
+                       sheet = "PPWORK", 
+                       skip = 9, 
+                       n_max = 19), silent = TRUE)
+  return(df)
+  
+}
+
+cleanEmp <- function(df){
+
+  
+}
+
+
+# Getting vaccination data
+getVacc <- function(dir_path, file){
+  #' Getting the vaccination sheet from the Axios-Ipsos survey data for a specific A/I "week"
+  #' Input: dir_path (chr), path to the data.
+  #'        file (chr), file name.
+  #' Output: df (data.frame), dataframe of the sheet containing the employment data from one A/I "week" 
+  
+  # Specify full path
+  fpath <- file.path(dir_path, file)
+  
+  # Get the name of the tabs in the spreadsheet.
+  tab_names <- excel_sheets(fpath)
+  
+  if ("Q73" %in% tab_names){
+    
+    # Read in relevant sheet.
+    df <- read_excel(fpath,
+                     sheet = "Q73",
+                     skip = 9,
+                     n_max = 34)
+    
+  } else {
+    print(glue("No sheet Q73 for {file}"))
+  }
+  
+  return(df)
+}
+
+
+
+cleanVacc <- function(df){
+  
+  
+}
+
+getAllSheets <- function(dir_path, ai_files, ai_periods, table){
+  #' 
+  #' Input: dir_path (chr), path to the data.
+  #'        ai_file (chr), all file names, e.g. ai_files[1] >> "Axios-Ipsos April 19 Crosstabs.xlsx"
+  #'        ai_periods (data.frame): period and end date of each A/I "week".
+  #'        table (chr): "emp" or "vacc", depending on which sheets you are getting. 
+  #' Output: df (data.frame), dataframe of the sheet containing the employment data from one A/I "week" 
+  
+  
+  # Which periods are unique
+  unique_periods <- ai_periods$Period[!ai_periods$Period %in% ai_periods$Period[duplicated(ai_periods$Period)]]
+  
+  for (file in ai_files){
+    
+    date <- str_match(file, pattern = " .*\\d")
+    date <- str_replace(date, " ", "")
+    
+    period <- ai_periods$Period[ai_periods$Date == date]
+    
+    # Determine name of file.
+    if (period %in% unique_periods){
+      df_name <- paste0(period,"_", table)
+    } else {
+      df_name <- paste0(period, "_", table, "_",1)
+      if (exists(df_name)){
+        df_name <- paste0(period, "_", table, "_",2)
+      }
+    }
+    
+    # Get the right sheet
+    if (table == "emp"){
+      df <- getEmp(dir_path, file)
+      # Clean df here
+      # df <- cleanEmp(df)
+    }
+    
+    if (table == "vacc"){
+      
+      df <- getVacc(dir_path, file)
+      # Clean df here
+      # df <- cleanVacc(df)
+    }
+    
+    assign(df_name, df, envir = parent.frame())
+    
+  }
+  
+}
+
+
+# Where is the data? (i.e. excel files)
+data_path <- file.path(rootDir,"Methods/Data/Surveys/Axios-Ipsos/Raw")
+
+# List all files
+ai_files <- list.files(data_path)
+
+getAllSheets(data_path, ai_files, ai_periods, "emp")
+getAllSheets(data_path, ai_files, ai_periods, "vacc")
+
+getIndices <- function(df, table){
+  
+  
+  
+}
+
+
